3c3a143f281118eaef9a054932ae850947e4a60c
mtvlc <mtvlcgithub@proton.me>
2022-10-09 13:14:59 -0300

    Started Challenge GUI

---
+++ World1/Challenge5/app.py
@@ -0,0 +1,15 @@
+# import system from os
+from os import system
+# importing graphic library
+from tkinter import *
+# application module
+def Application(self, master=None):
+    pass
+# application setup
+root = Tk()
+root.title('<&>')
+root.geometry('400x200')
+# application start
+Application(root)
+# mainloop
+root.mainloop()
