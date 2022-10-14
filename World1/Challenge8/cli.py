1c5af1c30a717b4de55ec61c9b05698d71d7af2d
mtvlc <mtvlcgithub@proton.me>
2022-10-13 22:52:55 -0300

    CLI done

--- World1/Challenge8/cli.py
+++ World1/Challenge8/cli.py
@@ -2,6 +2,30 @@
 from os import system
 # application module
 def Application():
-    pass
+    # clear terminal function
+    def Clear():
+        system('clear')
+    # result module
+    def Result():
+        print('{} metros equivalem a:\n'.format(meters))
+        # units
+        km = meters * 0.001
+        hm = meters * 0.01
+        dam = meters * 0.1
+        dm = meters * 10
+        cm = meters * 100
+        mm = meters * 1000
+        # kilometers
+        print('{} quilômetros.\n{} hectômetros.\n{} decâmetros.\n{} decímetros.\n{} centímetros.\n{} milímetros.'.format(km, hm, dam, dm, cm, mm))
+    Clear()
+    # call to action
+    print('Digite um valor em metros para mostrar as conversões.')
+    try:
+        meters = float(input('Metros: '))
+        Clear()
+        Result()
+    except ValueError:
+        Clear()
+        print('Erro: USB 01')
 # application start
 Application()
