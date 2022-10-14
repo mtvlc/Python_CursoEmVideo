e3f0c2c375a58ed91578c623968611c9f4f92aa0
mtvlc <mtvlcgithub@proton.me>
2022-10-13 23:17:26 -0300

    Started GUI

---
+++ World1/Challenge8/app.py
@@ -0,0 +1,15 @@
+# importing graphic library
+from tkinter import *
+# importing system from os
+from os import system
+# application module
+def Application(self, master=None):
+    pass
+# application setup
+root = Tk()
+root.title('lengthunit')
+root.geometry('400x200')
+# application start
+Application(root)
+# mainloop
+root.mainloop()
