61d84057873b324804c0994a239265937282b9d9
mtvlc <mtvlcgithub@proton.me>
2022-10-15 16:41:36 -0300

    CLI done

--- World1/Challenge9/cli.py
+++ World1/Challenge9/cli.py
@@ -5,6 +5,21 @@ def Application():
     # clear terminal function
     def Clear():
         system('clear')
+    # multiplication table
+    def Multiply():
+        Clear()
+        x = 0
+        print('Tabuada do número: {}'.format(userInput))
+        while x <= 10:
+            print('\033[1m{} x {:2d} = \033[33m{:2d}\033[m'.format(userInput, x, userInput * x))
+            x += 1
     Clear()
+    try:
+        # user input
+        userInput = int(input('Digite um número para ver sua tabuada.\nNúmero: '))
+        Multiply()
+    except ValueError:
+        Clear()
+        print('\033[1:31mErro: USB 01')
 # application start
 Application()
