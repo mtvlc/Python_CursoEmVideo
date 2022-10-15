8c46a5f62ae5c2d165dfdb81a4ca9c66859f8d8d
mtvlc <mtvlcgithub@proton.me>
2022-10-15 16:47:56 -0300

    Started GUI

---
+++ World1/Challenge9/app.py
@@ -0,0 +1,15 @@
+# importing system from os
+from os import system
+# importing graphic library
+from tkinter import *
+# application module
+def Application(self, master=None):
+    pass
+# application setup
+root = Tk()
+root.title('Multiplication')
+root.geometry('400x350')
+# application start
+Application(root)
+# mainloop
+root.mainloop()
