f59662ab60e06a7491fde201ce79245bdb52eca4
mtvlc <mtvlcgithub@proton.me>
2022-10-14 23:45:38 -0300

    Started CLI

---
+++ World1/Challenge9/cli.py
@@ -0,0 +1,10 @@
+# import system from os
+from os import system
+# application module
+def Application():
+    # clear terminal function
+    def Clear():
+        system('clear')
+    Clear()
+# application start
+Application()
