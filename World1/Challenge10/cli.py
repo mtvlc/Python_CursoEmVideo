d939d56239423ef88b7948d7bb2fc23ab58c0a87
mtvlc <mtvlcgithub@proton.me>
2022-10-16 14:52:50 -0300

    CLI done

--- World1/Challenge10/cli.py
+++ World1/Challenge10/cli.py
@@ -1,3 +1,7 @@
+# importing requests
+import requests
+# importing json
+import json
 # importing system from os
 from os import system
 # application module
@@ -5,6 +9,21 @@ def Application():
     # clear function
     def Clear():
         system('clear')
+    # get dolar price
+    def Dolar(brl):
+        # access api
+        api = requests.get('https://economia.awesomeapi.com.br/all/USD-BRL')
+        # api return
+        value = api.json()
+        # price
+        price = value['USD']['bid']
+        Clear()
+        print('Com \033[32mR${:.2f}\033[m você pode comprar \033[32mUS${:.2f}\033[m.'.format(brl, brl / float(price)))
     Clear()
+    try:
+        brl = float(input('Ensira um valor em reais para converter para dólares americanos.\nR$:'))
+        Dolar(brl)
+    except ValueError:
+        print('\033[1:31mErro: USB 01')
 # application start
 Application()
