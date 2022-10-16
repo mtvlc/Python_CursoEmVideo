f55e7fd79db6e668852463fa6105c2cee04efcd4
mtvlc <mtvlcgithub@proton.me>
2022-10-16 13:55:16 -0300

    Challenge complete!

--- World1/Challenge9/app.py
+++ World1/Challenge9/app.py
@@ -4,11 +4,67 @@ from os import system
 from tkinter import *
 # application module
 def Application(self, master=None):
-    pass
+    # reset function
+    def Reset():
+        self.result.destroy()
+        self.apply['command'] = Result
+        self.reset.pack_forget()
+    # menu module
+    def Menu():
+        # menu widget
+        self.menu = Frame(self.top)
+        self.menu.pack(pady=(0, 5))
+        # exit button
+        exit = Button(self.menu, command=self.top.quit, text='Sair')
+        exit.pack(side=LEFT)
+        # apply button
+        self.apply = Button(self.menu, command=Result, text='Calcular')
+        self.apply.pack(side=LEFT)
+        # reset button
+        self.reset = Button(self.menu, command=Reset, text='Voltar')
+    # starting window
+    def Start():
+        # starting window widget
+        self.starting = Frame(self.top)
+        self.starting.pack()
+        # call to action
+        cta = Label(self.starting, font=('', 15), text='Ensira um número abaixo em seguida\nclique em calcular para ver a tabuada\ndo número inserido.')
+        cta.pack(pady=(5, 5))
+        # user input
+        self.input = Entry(self.starting, width=15)
+        self.input.insert(END, 10)
+        self.input.pack(pady=(5, 10))
+        Menu()
+    # result function
+    def Result():
+        self.result = Frame(self.top)
+        self.result.pack()
+        try:
+            number = int(self.input.get())
+            x = 0
+            self.apply['command'] = ''
+            # result widget
+            while x <= 10:
+                self.n = Label(self.result, fg='orange', font=('', 10, 'bold'), text='{:2d} x {:2d} = {:2d}'.format(number, x, number * x))
+                self.n.pack()
+                x += 1
+            self.reset.pack()
+        except ValueError:
+            error = Label(self.result, fg='red', font=('', 15, 'bold'), text='Erro: USB 01')
+            error.pack()
+            self.apply['command'] = ''
+            self.reset.pack()
+    # terminal clear
+    system('clear')
+    # top frame
+    self.top = Frame(master)
+    self.top.pack()
+    # start window
+    Start()
 # application setup
 root = Tk()
 root.title('Multiplication')
-root.geometry('400x350')
+root.geometry('400x400')
 # application start
 Application(root)
 # mainloop
