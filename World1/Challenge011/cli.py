25000d62522a2c0a675ba46c711f977dc253147a
mtvlc <mtvlcgithub@proton.me>
2022-10-17 20:44:06 -0300

    CLI done

--- World1/Challenge011/cli.py
+++ World1/Challenge011/cli.py
@@ -6,5 +6,18 @@ def Application():
     def Clear():
         system('clear')
     Clear()
+    # call to action
+    print('Ensira as dimensões (largura, altura) da parede para calcular a quantidade de tinta necessária para pintá-la.')
+    try:
+        # user input
+        width = float(input('Largura: '))
+        height = float(input('Altura: '))
+        area = width * height
+        Clear()
+        # result
+        print('Para uma área de \033[33m{}\033[mm² serão necessários \033[33m{}\033[ml de tinta.'.format(area, area / 2))
+    except ValueError:
+        Clear()
+        print('\033[1:31mErro: USB 01')
 # application start
 Application()
