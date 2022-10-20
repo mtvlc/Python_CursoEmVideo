d7453c6c1582554675c74e6edf947e1cccc99483
mtvlc <mtvlcgithub@proton.me>
2022-10-19 23:14:10 -0300

    Started CLI

---
+++ World1/Challenge012/cli.py
@@ -0,0 +1,11 @@
+# importing system from os
+from os import system
+# application module
+def Application():
+    # clear function # clear terminal
+    def Clear():
+        system('clear')
+    Clear()
+# ensira o preço do produto que deseja calcular o desconto.
+# application start
+Application()
