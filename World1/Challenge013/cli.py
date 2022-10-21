08f56d851ace79b13c47e5d939efa4a129feb2c4
mtvlc <mtvlcgithub@proton.me>
2022-10-21 02:53:11 -0300

    CLI done

--- World1/Challenge013/cli.py
+++ World1/Challenge013/cli.py
@@ -7,5 +7,20 @@ def Application():
         system('clear')
     # clean terminal at program starts
     Clear()
+    try:
+        # user input salary
+        salary = float(input('Ensira seu salário para calcular o aumento.\nSalário: R$'))
+        # clean terminal
+        Clear()
+        # print current salary value
+        print('O valor do seu salário atual é = R$\033[33m{:.2f}\033[m.'.format(salary))
+        # print salary value after increase 15%
+        print('O valor do salário após 15% de aumento R$\033[32m{:.2f}\033[m.'.format(salary * 115 / 100))
+        # print amount increased
+        print('Valor do aumento = R$\033[32m{:.2f}\033[m.'.format(salary * 115 / 100 - salary))
+    except ValueError:
+        # clean terminal and print error code
+        Clear()
+        print('\033[1:31mErro: USB 01')
 # application start
 Application()
