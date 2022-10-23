e0286b3f4b9e0067bf815d1746fe5b6cf9670146
mtvlc <mtvlcgithub@proton.me>
2022-10-23 14:15:42 -0300

    Challenge 014 CLI done

--- World1/Challenge014/cli.py
+++ World1/Challenge014/cli.py
@@ -7,5 +7,19 @@ def Application():
         system('clear')
     # clear terminal at program starts
     Clear()
+    # try to get a valid input and print result
+    try:
+        # get degrees celsius
+        celsius = float(input('Ensira uma temperatura para convertê-la para ºF.\nCelcius: º'))
+        # clear terminal
+        Clear()
+        if celsius == 1:
+            print('\033[1:33m{:.1f}\033[mºC equivale a \033[1:33m{:.1f}\033[mºF.'.format(celsius, celsius * 1.800 + 32.00))
+        else:
+            print('\033[1:33m{:.1f}\033[mºC equivalem a \033[1:33m{:.1f}\033[mºF.'.format(celsius, celsius * 1.800 + 32.00))
+    # clean terminal and print error message
+    except ValueError:
+        Clear()
+        print('\033[1:31mErro: USB 01\033[m')
 # application start
 Application()
