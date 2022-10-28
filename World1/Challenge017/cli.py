dcdbdcd8aa308f7798e70566d4da53aaf8cddc22
mtvlc <mtvlcgithub@proton.me>
2022-10-28 11:52:45 -0300

    Challenge 017 CLI done

--- World1/Challenge017/cli.py
+++ World1/Challenge017/cli.py
@@ -2,10 +2,21 @@
 from os import system
 # application module
 def Application():
-    # clean function # clear terminal
+    # clear function # clear terminal
     def Clear():
         system('clear')
-    # clean teminal at promgram starts
+    # clean terminal at program starts
     Clear()
+    # description
+    print('Ensira o cateto oposto e o cateto adjacente de um triângulo retângulo para calcular a hipotenusa.')
+    try:
+        catetoOposto = float(input('Cateto Oposto: '))
+        catetoAdjacente = float(input('Cateto Adjacente: '))
+        hipotenusa = pow(pow(catetoOposto, 2.0) + pow(catetoAdjacente, 2.0), 0.5)
+        Clear() # clean terminal
+        print('O comprimento da hipotenusa é = \033[1:33m{:.2f}\033[m.'.format(hipotenusa))
+    except ValueError:
+        Clear()
+        print('\033[1:31mErro: USB 01\033[m')
 # application start
 Application()
