a483a1b8015eea4bc07824848b72372ef124c5f5
mtvlc <mtvlcgithub@proton.me>
2022-10-28 10:31:02 -0300

    Started GUI Challenge 016

---
+++ World1/Challenge016/app.py
@@ -0,0 +1,18 @@
+# importing truncate from math
+from math import trunc
+# importing system from os
+from os import system
+# importing graphic library
+from tkinter import *
+# application module
+def Application(self, master=None):
+    # clean terminal at program starts
+    system('clear')
+# application setup
+root = Tk()
+root.title('Truncate')
+root.geometry('400x200')
+# application start
+Application(root)
+# mainloop
+root.mainloop()
