583dae044e5252aa9638386794a8c929b12140b1
mtvlc <mtvlcgithub@proton.me>
2022-10-29 02:54:31 -0300

    Challenge 018 CLI done

--- World1/Challenge018/cli.py
+++ World1/Challenge018/cli.py
@@ -9,5 +9,13 @@ def Application():
         system('clear')
     # clean terminal at program starts
     Clear()
+    try: # get angle and print sen, cos, and tan
+        angle = float(input('Ensira um ângulo para mostrar seu \033[33mseno\033[m, \033[33mcosseno\033[m e \033[33mtangente\033[m:'))
+        Clear()
+        print('Ângulo de {:.1f}º'.format(angle))
+        print('Seno     = {:.2f}\nCosseno  = {:.2f}\nTangente = {:.2f}'.format(sin(radians(angle)), cos(radians(angle)), tan(radians(angle))))
+    except ValueError: # clean terminal and print error code
+        Clear()
+        print('\033[1:31mErro: USB 01\033[m')
 # application start
 Application()
