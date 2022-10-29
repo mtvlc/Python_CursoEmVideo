b7e2c9c543f581c8baa80f123870e120deb2840d
mtvlc <mtvlcgithub@proton.me>
2022-10-29 02:36:17 -0300

    Started CLI Challenge 018

---
+++ World1/Challenge018/cli.py
@@ -0,0 +1,13 @@
+# importing radians, sin, cos, tan from math
+from math import radians, sin, cos, tan
+# importing system from os
+from os import system
+# application module
+def Application():
+    # clear function # clean terminal
+    def Clear():
+        system('clear')
+    # clean terminal at program starts
+    Clear()
+# application start
+Application()
