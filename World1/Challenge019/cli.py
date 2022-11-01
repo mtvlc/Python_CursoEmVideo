52d31d80ada0fe4d2c2e83d2e9c35c544722a682
mtvlc <mtvlcgithub@proton.me>
2022-11-01 14:34:11 -0300

    Challenge 019 CLI done

--- World1/Challenge019/cli.py
+++ World1/Challenge019/cli.py
@@ -8,5 +8,14 @@ def Application():
     def Clear(): # clean terminal
         system('clear')
     Clear() # clean terminal at program starts
+    students = [] # students list
+    while len(students) < 4:
+        name = str(input('Ensira o nome do {}º aluno(a): '.format(len(students) + 1))) # get student name
+        students.append(name) # insert student name in students list
+        Clear() # clean terminal
+    print('Alunos selecionados:')
+    for x in students: # print students list names
+        print(x)
+    print('\nO aluno sorteado foi \033[1:33m{}\033[m!'.format(choice(students)))
 # application start
 Application()
