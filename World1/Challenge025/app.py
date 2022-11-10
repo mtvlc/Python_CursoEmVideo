61197892145ea8c23f262a47a52d440fe4601103
mtvlc <mtvlcgithub@proton.me>
2022-11-10 16:09:42 -0300

    Started GUI Challenge 025

---
+++ World1/Challenge025/app.py
@@ -0,0 +1,17 @@
+# importing system from os
+from os import system
+# importing graphic library
+from tkinter import *
+# importing tkinter.messagebox
+import tkinter.messagebox
+# application module
+def Application(self, master=None):
+    system('clear') # clean terminal at program starts
+# application setup
+root = Tk()
+root.title('SilvaChecker')
+root.geometry('400x200')
+# application start
+Application(root)
+# mainloop
+root.mainloop()
