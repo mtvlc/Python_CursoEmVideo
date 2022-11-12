dd243e7ecfb40850c103796f231eba2aadb2958a
mtvlc <mtvlcgithub@proton.me>
2022-11-12 16:32:43 -0300

    Challenge 026 CLI done

--- World1/Challenge026/cli.py
+++ World1/Challenge026/cli.py
@@ -6,5 +6,13 @@ def Application():
     def Clear():
         system('clear') # clean terminal
     Clear() # clear terminal at program starts
+    phrase = str(input('insira algum texto: '))
+    Clear() # clear terminal
+    # count A
+    print('No texto insirido a letra "\033[1mA\033[m" aparece \033[1:33m{}\033[m vezes.'.format(phrase.upper().count('A')))
+    # first
+    print('Ela aparece pela primeira vez na posição \033[1:33m{}\033[m.'.format(phrase.upper().find('A') + 1))
+    # last
+    print('Ela aparece pela última vez na posição \033[1:33m{}\033[m.'.format(phrase.upper().rfind('A') + 1))
 # application start
 Application()
