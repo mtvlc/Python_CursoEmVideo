c63ce2b47dc0caef86150b655b2dcabb63d68a12
mtvlc <mtvlcgithub@proton.me>
2022-11-14 08:46:53 -0300

    Started CLI Challenge 028

---
+++ World1/Challenge028/cli.py
@@ -0,0 +1,12 @@
+# importing system from os
+from os import system
+# importing randint from random
+from random import randint
+# application module
+def Application():
+    # clear function
+    def Clear():
+        system('clear') # clear terminal
+    Clear() # clear terminal at program starts
+# application start
+Application()
