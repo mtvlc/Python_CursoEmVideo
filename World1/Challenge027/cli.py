a18ed35ff0c6ad19a61c23e47fd56fa68045c0b7
mtvlc <mtvlcgithub@proton.me>
2022-11-14 07:40:53 -0300

    Challenge 027 CLI done

--- World1/Challenge027/cli.py
+++ World1/Challenge027/cli.py
@@ -6,5 +6,10 @@ def Application():
     def Clear():
         system('clear') # clean terminal
     Clear() # clean terminal at program starts
+    # get name
+    name = str(input('Nome Completo: ')).strip().title().split()
+    Clear() # clear terminal
+    # result
+    print('\033[1mPrimeiro nome:\033[m \033[1:33m{}\033[m\n\033[1m  Último nome: \033[33m{}\033[m'.format(name[0], name[-1]))
 # application start
 Application()
