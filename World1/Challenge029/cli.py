02fcb3b2674d3f6391aba6de09785f31eddee7b7
mtvlc <mtvlcgithub@proton.me>
2022-11-19 20:26:21 -0300

    Challenge 029 CLI done

--- World1/Challenge029/cli.py
+++ World1/Challenge029/cli.py
@@ -6,5 +6,18 @@ def Application():
     def Clear():
         system('clear') # clean terminal
     Clear() # clean terminal at program starts
+    # speed limit
+    speedLimit = 80
+    try:
+        # user input vehicle speed
+        vehicleSpeed = int(input('Insira a velocidade que o veículo estava: '))
+        Clear() # clean terminal
+        if vehicleSpeed > speedLimit:
+            print('\033[1:31mVeículo acima do limite permitido!\033[m\n\033[33mValor da multa: R$\033[1m{:.2f}\033[m\033[33m.\033[m'.format((vehicleSpeed - speedLimit) * 7))
+        else:
+            print('\033[32mVeículo dentro do limite de velocidade.\033[m')
+    except ValueError:
+        Clear() # clean terminal
+        print('\033[1:31mErro: USB 01\033[m')
 # application start
 Application()
