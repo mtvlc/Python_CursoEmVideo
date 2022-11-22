4dd1f6416d38b82d3269a5d493f2c23cd6323e26
mtvlc <mtvlcgithub@proton.me>
2022-11-21 23:01:18 -0300

    Started CLI Challenge 032

---
+++ World1/Challenge032/cli.py
@@ -0,0 +1,12 @@
+# importing system from os
+from os import system
+# importing time module
+import time
+# application module
+def Application():
+    # clear function
+    def Clear():
+        system('clear') # clean terminal
+    Clear() # clean terminal at program starts
+# applicaton start
+Application()
