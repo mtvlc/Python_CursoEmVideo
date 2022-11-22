61b414a337228bf9c5f303dfe6c4d37c8d641413
mtvlc <mtvlcgithub@proton.me>
2022-11-22 19:12:17 -0300

    Started GUI Challenge 032

---
+++ World1/Challenge032/app.py
@@ -0,0 +1,19 @@
+# importing system from os
+from os import system
+# importing graphic library
+from tkinter import *
+# importing tkinter.messagebox
+import tkinter.messagebox
+# importing datetime from datetime
+from datetime import datetime
+# application module
+def Application(self, master=None):
+    system('clear') # clean terminal at program starts
+# application setup
+root = Tk()
+root.title('LeapYear')
+root.geometry('400x200')
+# application start
+Application(root)
+# mainloop
+root.mainloop()
