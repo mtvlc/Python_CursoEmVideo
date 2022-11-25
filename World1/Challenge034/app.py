7b2edef1f23eb0445f94c3e6e20328d0dc94098b
mtvlc <mtvlcgithub@proton.me>
2022-11-25 13:55:55 -0300

    Started GUI Challenge 034

---
+++ World1/Challenge034/app.py
@@ -0,0 +1,10 @@
+from os import system # importing system from os
+from tkinter import * # importing graphic library
+import tkinter.messagebox # importing tkinter.messagebox
+def Application(self, master=None): # application module
+    system('clear') # clean terminal at program starts
+root = Tk() # tkinter window
+root.title('Salary') # window title
+root.geometry('400x200') # window size
+Application(root) # start application
+root.mainloop() # call mainloop method
